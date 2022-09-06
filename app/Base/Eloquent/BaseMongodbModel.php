694e73551b65cb87fc1a63eb03b4e6aaa517f692
detyar <yaroslav.decik@gmail.com>
2022-09-06 19:34:13 +0300

    mongo off

--- app/Base/Eloquent/BaseMongodbModel.php
+++ app/Base/Eloquent/BaseMongodbModel.php
@@ -1,14 +1,14 @@
 <?php
-
-namespace App\Base\Eloquent;
-
-use Jenssegers\Mongodb\Eloquent\Model;
-use DateTimeInterface;
-
-class BaseMongodbModel extends Model
-{
-    protected function serializeDate(DateTimeInterface $date)
-    {
-        return $date->format('Y-m-d H:i:s');
-    }
-}
+//
+//namespace App\Base\Eloquent;
+//
+//use Jenssegers\Mongodb\Eloquent\Model;
+//use DateTimeInterface;
+//
+//class BaseMongodbModel extends Model
+//{
+//    protected function serializeDate(DateTimeInterface $date)
+//    {
+//        return $date->format('Y-m-d H:i:s');
+//    }
+//}
