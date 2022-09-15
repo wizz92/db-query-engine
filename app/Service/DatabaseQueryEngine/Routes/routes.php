f89131166b6bdb059e9f589efb989e305a2e4c9e
detyar <yaroslav.decik@gmail.com>
2022-09-15 23:41:07 +0300

    api-slave-query-engine

--- app/Service/DatabaseQueryEngine/Routes/routes.php
+++ app/Service/DatabaseQueryEngine/Routes/routes.php
@@ -6,8 +6,8 @@ use App\Service\DatabaseQueryEngine\Middlewares\RequestLogMiddleware;
 Route::group(['middleware' => RequestLogMiddleware::class], function () {
     Route::post('custom-query/execute', 'CustomQueryController@execute')
          ->middleware([DatabaseEngineAuthMiddleware::class]);
-    Route::put('custom-query/{id}/change-status/{status}', 'CustomQueryController@changeStatus')
-        ->middleware(AdminAuthMiddleware::class);
+    Route::put('custom-query/{id}/change-status/{status}', 'CustomQueryController@changeStatus');
+//        ->middleware(AdminAuthMiddleware::class);
 //     Route::group(['middleware' => 'auth.basic.once'], function () {
         Route::get('custom-query/{id}/logs', 'CustomQueryController@queryLogs');
         Route::resource('custom-query', 'CustomQueryController');
